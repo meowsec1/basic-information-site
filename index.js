c2207b4bd23f607a12655179d1017a6737df41e6
v <128931588+meowsec1@users.noreply.github.com>
2025-09-30 01:46:07 +0700

    Made file reading asynchronous, rather than blocking synchronously at the start. Switched from hardcoded switch statement route handlers to handling routes dynamically based off the URL.

--- index.js
+++ index.js
@@ -1,35 +1,25 @@
 const http = require('node:http');
 const fs = require('node:fs');
+const path = require('path');
 
-
-let homeContents, aboutContents, contactContents, notFoundContents;
-
-
-try {
-    homeContents = fs.readFileSync('./pages/index.html', 'utf8');
-    aboutContents = fs.readFileSync('./pages/about.html', 'utf8');
-    contactContents = fs.readFileSync('./pages/contact-me.html', 'utf8');
-    notFoundContents = fs.readFileSync('./pages/404.html', 'utf8');
-} catch (error) {
-    console.log("Something went wrong: ", error);
-}
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    switch(req.url) {
-        case '/':
-            res.end(homeContents);
-            break;
-        case '/about':
-            res.end(aboutContents);
-            break;
-        case '/contact-me':
-            res.end(contactContents);
-            break;
-        default:
-            res.end(notFoundContents);
-            break;
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    if (path.extname(url.pathname)) {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end(fs.readFileSync('./pages/404.html', 'utf8'));
+        return;
     }
+    fs.readFile('pages' + (url.pathname === ('/' || '') ? '/index' : url.pathname) + '.html', 'utf8', (err, data) => {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            res.end(fs.readFileSync('./pages/404.html', 'utf8'));
+            return;
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+        return;
+    })
 });
 
 server.listen(8080);
